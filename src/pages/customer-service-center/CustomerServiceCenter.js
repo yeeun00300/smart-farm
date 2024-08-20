595074119b44762a9cfdc0476db20fba37aa5b93
yeeun <mlikshoot99@gmail.com>
2024-08-20 12:46:21 +0900

    update

--- src/pages/customer-service-center/CustomerServiceCenter.js
+++ src/pages/customer-service-center/CustomerServiceCenter.js
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import styles from "./CustomerServiceCenter.module.scss";
+import React from "react";
 // import up from "/assets/up-arrow_icon-icons.com_73351.svg";
 import Faq from "./faq/Faq";
 
@@ -8,32 +7,7 @@ import Faq from "./faq/Faq";
 // angle-arrow-down_icon-icons.com_73683.svg - down 화살표에 해당하는 이미지
 
 function CustomerServiceCenter() {
-  // 초기 상태는 false로 .answer(답변)을 숨깁니다.
-  const [isVisible, setIsVisible] = useState(false);
-
-  // .detail(버튼)을 누를 때의 함수입니다.
-  const toggleVisibility = () => {
-    setIsVisible((prevState) => !prevState);
-  };
-
-  return (
-    <>
-      <div className={styles.faq}>
-        <div className={styles.question}>
-          <h3>제목1</h3>
-          <button className={styles.detail} onClick={toggleVisibility}>
-            눌러보셈
-          </button>
-        </div>
-        {isVisible && (
-          <div className={styles.answer}>
-            <h4>description</h4>
-          </div>
-        )}
-      </div>
-      <div className={styles.faq}>{/* <Faq /> */}</div>
-    </>
-  );
+  return <Faq />;
 }
 
 export default CustomerServiceCenter;
--- src/pages/customer-service-center/faq/Faq.js
+++ src/pages/customer-service-center/faq/Faq.js
@@ -1,10 +1,32 @@
-import React from "react";
-import { useDispatch } from "react-redux";
+import React, { useState } from "react";
+import styles from "./Faq.module.scss";
 
 function Faq({ title, description }) {
-  const dispatch = useDispatch();
+  // 초기 상태는 false로 .answer(답변)을 숨깁니다.
+  const [isVisible, setIsVisible] = useState(false);
 
-  return <div></div>;
+  // .detail(버튼)을 누를 때의 함수입니다.
+  const toggleVisibility = () => {
+    setIsVisible((prevState) => !prevState);
+  };
+
+  return (
+    <>
+      <div className={styles.faq}>
+        <div className={styles.question}>
+          <h3>제목1</h3>
+          <button className={styles.detail} onClick={toggleVisibility}>
+            눌러보셈
+          </button>
+        </div>
+        {isVisible && (
+          <div className={styles.answer}>
+            <h4>description</h4>
+          </div>
+        )}
+      </div>
+    </>
+  );
 }
 
 export default Faq;

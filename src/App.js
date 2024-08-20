61edc9377235c6e531523dc9dd60ce2e9a15d9c9
yeeun <mlikshoot99@gmail.com>
2024-08-20 10:21:55 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Layout from './components/layout/Layout';
-import LoginPage from './pages/LoginPage/LoginPage';
-import About from './pages/about/About';
-import Home from './pages/home/Home';
-import Simulation from './pages/simulation/Simulation';
-import Detail from './pages/simulation/details/Detail';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./components/layout/Layout";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import About from "./pages/about/About";
+import Home from "./pages/home/Home";
+import Simulation from "./pages/simulation/Simulation";
+import Detail from "./pages/simulation/details/Detail";
+import CustomerServiceCenter from "./pages/customer-service-center/CustomerServiceCenter";
 
 function App() {
   return (
@@ -18,6 +19,10 @@ function App() {
             <Route path=":id" element={<Detail />} />
           </Route>
           <Route path="Login" element={<LoginPage />} />
+          <Route
+            path="customer-service-center"
+            element={<CustomerServiceCenter />}
+          />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/pages/customer-service-center/CustomerServiceCenter.js
+++ src/pages/customer-service-center/CustomerServiceCenter.js
@@ -1,7 +1,15 @@
 import React from "react";
+import "./CustomerServiceCenter.module.scss";
 
 function CustomerServiceCenter() {
-  return <div>고객센터입니다.</div>;
+  return (
+    <>
+      <div className="faq">
+        <h3>질문?</h3>
+        <h4>대답</h4>
+      </div>
+    </>
+  );
 }
 
 export default CustomerServiceCenter;

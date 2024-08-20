f67e10dce5d8b5f67ff0067be8ec468b1d16111d
yeeun <mlikshoot99@gmail.com>
2024-08-20 12:21:17 +0900

    update

--- src/pages/customer-service-center/CustomerServiceCenter.js
+++ src/pages/customer-service-center/CustomerServiceCenter.js
@@ -1,13 +1,45 @@
-import React from "react";
-import "./CustomerServiceCenter.module.scss";
+import React, { useState } from "react";
+import styles from "./CustomerServiceCenter.module.scss";
+// import Faq from "./faq/Faq";
 
 function CustomerServiceCenter() {
+  // 초기 상태는 false로 .answer(답변)을 숨깁니다.
+  const [isVisible, setIsVisible] = useState(false);
+
+  // .detail(버튼)을 누를 때의 함수입니다.
+  const toggleVisibility = () => {
+    setIsVisible((prevState) => !prevState);
+  };
+
   return (
     <>
-      <div className="faq">
-        <h3>질문?</h3>
-        <h4>대답</h4>
+      <div className={styles.faq}>
+        <div className={styles.question}>
+          <h3>제목1</h3>
+          <button className={styles.detail} onClick={toggleVisibility}>
+            눌러보셈
+          </button>
+        </div>
+        {isVisible && (
+          <div className={styles.answer}>
+            <h4>description</h4>
+          </div>
+        )}
       </div>
+      <div className={styles.faq}>
+        <div className={styles.question}>
+          <h3>제목2</h3>
+          <button className={styles.detail} onClick={toggleVisibility}>
+            눌러보셈
+          </button>
+        </div>
+        {isVisible && (
+          <div className={styles.answer}>
+            <h4>description</h4>
+          </div>
+        )}
+      </div>
+      {/* <Faq /> */}
     </>
   );
 }
---
+++ src/pages/customer-service-center/faq/Faq.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+
+function Faq({ title, description }) {
+  const dispatch = useDispatch();
+
+  return <div></div>;
+}
+
+export default Faq;
